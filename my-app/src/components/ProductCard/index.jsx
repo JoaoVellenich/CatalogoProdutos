23955818ccc9b90700a5b3d829e034717c313305
Joao Pedro Hegedus Vellenich <jpvellenich@gmail.com>
2021-09-22 15:39:27 -0300

    api.PUT

--- my-app/src/components/ProductCard/index.jsx
+++ my-app/src/components/ProductCard/index.jsx
@@ -9,6 +9,25 @@ export const ProductCard = ({name, image, id, price, description}) =>{
         window.location.href='/';
     }
 
+    async function handleEdit(){
+        const newName = prompt('New Name:', name);
+        const newDescription = prompt('New Description:', description);
+        const newImageUrl = prompt('New imageUrl:', image);
+        const newPrice = prompt('New Price:', price);
+
+        if(newName !==null && newDescription!==null && newImageUrl!==null &&newPrice!==null){
+            const data = {
+                name:newName, 
+                description:newDescription, 
+                price:newPrice, 
+                imageUrl:newImageUrl
+            }
+    
+            api.put('/api/product/'+id, data);
+            window.location.href='/';
+        }
+    }
+
     return (
         <div className='Containter'>
             <h3 className='prodc-name'>{name}</h3>
@@ -18,7 +37,7 @@ export const ProductCard = ({name, image, id, price, description}) =>{
                 <p className='Description'>{description}</p>
             </div>
             <button onClick={handleDelete} className='Button-Del'>Delete</button>
-            <button  className='Button-Edit'>Edit</button>
+            <button onClick={handleEdit} className='Button-Edit'>Edit</button>
         </div>
     );
 }
